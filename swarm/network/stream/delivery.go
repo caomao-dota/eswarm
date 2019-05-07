5bb73a802065688a6f0cc5f1f391f8dd045aa730
CsdcFund <CsdcFund@gmail.com>
2019-05-07 22:06:09 +0700

    bug修正，连接速度问题，当设备加入到bucket中时，如果没有注册，执行一次addNodeHook

--- swarm/network/stream/delivery.go
+++ swarm/network/stream/delivery.go
@@ -178,7 +178,7 @@ func (d *Delivery) GetReceivedChunkInfo() map[common.Address]int64{
 
 //收到了某个节点来的查询数据的请求
 func (d *Delivery) handleRetrieveRequestMsg(ctx context.Context, sp *Peer, req *RetrieveRequestMsg) error {
-	log.Info("received request", "peer", sp.ID(), "hash", req.Addr)
+	log.Trace("received request", "peer", sp.ID(), "hash", req.Addr)
 	handleRetrieveRequestMsgCount.Inc(1)
 
 	//记录
@@ -343,7 +343,7 @@ func (d *Delivery) RequestFromPeers(ctx context.Context, req *network.Request) (
 	// this span will finish only when delivery is handled (or times out)
 	ctx = context.WithValue(ctx, tracing.StoreLabelId, "stream.send.request")
 	ctx = context.WithValue(ctx, tracing.StoreLabelMeta, fmt.Sprintf("%v.%v", sp.ID(), req.Addr))
-	log.Info("Send Request:","addr",req.Addr,"Hop",req.HopCount,"target:",sp.ID())
+	log.Trace("Send Request:","addr",req.Addr,"Hop",req.HopCount,"target:",sp.ID())
 	err := sp.SendPriority(ctx, &RetrieveRequestMsg{
 		Addr:      req.Addr,
 		SkipCheck: req.SkipCheck,
